aef5727aecd85e7552ffa5b1cf38b05fec494c6c
Pavithra-2006-rgb <murthypavithra4@gmail.com>
2026-03-18 15:06:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify, session
 import re, hashlib, json
 from datetime import datetime, timedelta
@@ -594,4 +595,4 @@ def parse_syllabus(text):
     return units
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
